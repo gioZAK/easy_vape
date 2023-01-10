1a1aa086f3a865111c1bb31047187b40cb7d9b08
gioZAK <giovane.zakc@gmail.com>
2023-01-10 16:50:58 +0000

    Add 404 handler functionality

--- easy_vape/urls.py
+++ easy_vape/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import handler404
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
@@ -12,3 +14,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'easy_vape.views.handler404'
