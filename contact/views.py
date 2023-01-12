6829e231f21e8b137fa974ef27b78d2b02c7fb0a
gioZAK <giovane.zakc@gmail.com>
2023-01-12 17:24:32 +0000

    Add views, url path and templates to the contact app

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .forms import ContactForm
 
-# Create your views here.
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            # send email
+            send_mail(
+                subject,
+                message,
+                email,
+                ['giovane.zakc@gmail.com'],
+                fail_silently=False,
+            )
+            form.save()
+            request.session['contact'] = form.cleaned_data
+            return redirect(reverse('contact:contact_success'))
+
+    else:
+        form = ContactForm(user=request.user)
+    return render(request, 'contact/contact.html', {'form': form})
+
+
+def contact_success(request):
+    return render(request, 'contact/contact_success.html',
+                  {'contact': request.session['contact'],
+                   'user': request.user})
