c3d0f7c6c0a8a71f0473124209785f42f69a6355
gioZAK <giovane.zakc@gmail.com>
2023-01-12 17:48:25 +0000

    Update contact templates and setup admin control

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'email', 'subject')
+
+
+admin.site.register(Contact, ContactAdmin)
